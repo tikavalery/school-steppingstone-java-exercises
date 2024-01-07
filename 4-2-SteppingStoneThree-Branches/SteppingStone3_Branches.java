85cecf91d7c6f4fc45ee08361dbb673d9507f3db
valery <tikavalery@gmail.com>
2024-01-06 17:58:23 -0800

    added some new project folder and some new files

--- 4-2-SteppingStoneThree-Branches/SteppingStone3_Branches.java
+++ 4-2-SteppingStoneThree-Branches/SteppingStone3_Branches.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package SteppingStones;
+ package SteppingStones;
 
 import java.util.Scanner;
 
@@ -12,10 +12,11 @@ public class SteppingStone3_Branches {
     
     public static void main(String[] args) {
        int numberCups = -1;
+
        /**
         * Add a CONSTANT variable MAX_CUPS assigned to the value 100
         */
-        
+      final int MAX_CUPS = 100;  
        
        Scanner scnr = new Scanner(System.in);
        
@@ -24,8 +25,35 @@ public class SteppingStone3_Branches {
        //The following "if branch" uses the scanner method hasNextInt() to
        //check to see if the input is an int.
        if (scnr.hasNextInt()) {
+
+        //here we get the input of the user using the scrn.netInt() object and asign in to the numberCups variable
            numberCups = scnr.nextInt();
+
+        //we then use an if statement to compare the number of cups entered to the MAX_CUPS variable.If the number entered is between 1 and 
+        //100 we then print and acknowledgement statement.
+        //if the number is not between 1 and 100 we use the scanner object to ask the user to input another number 
+           if(numberCups <= MAX_CUPS ){
+            System.out.println(numberCups + " is a valid number of cups!");
+           } else{
+            System.out.println(numberCups + " is not a valid number of cups!");
+              //if the number is not between 1 and 100 we use the scanner object to ask the user to input another number between 1 and 100
+            System.out.println("Please enter another number of cups between 1 and 100: ");
+            //below we use the scanner object to get the user input from the terminal and store it in the numberCups variable
+            numberCups = scnr.nextInt();
+            //We now compare the number of cup entered by the user to the max cups again to make sure it is with the range of 1 to 100
+             if(numberCups <= MAX_CUPS){
+                //if within range we print an acknowledgement statement
+                System.out.println(numberCups + " is a valid number od cups!");
+             } else if(numberCups < 1){
+                //if not we print a statement telling user he is out of attemps
+                System.out.println(numberCups + " is less than 1. Sorry you are out od attemps");
+             } else{
+                 //if not we print a statement telling user he is out of attemps
+                System.out.println(numberCups + " is greater than 100. Sorry you are out of attemps");
+             }
+           }
            
+
 	   /**NESTED BRANCH:
             * Insert a nested branch that follows the following pattern:
             * 
@@ -51,7 +79,7 @@ public class SteppingStone3_Branches {
             *
             */
                  
-           }
+           
        } else {
            System.out.println("Error: That is not a number. Try again.");
        }
--- 5-2-SteppingStoneFour-Loops/SteppingStone4_Loops.java
+++ 5-2-SteppingStoneFour-Loops/SteppingStone4_Loops.java
@@ -1,4 +1,4 @@
-package SteppingStones;
+// package SteppingStones;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -21,10 +21,24 @@ public class SteppingStone4_Loops {
        System.out.println("Please enter the recipe name: ");
        recipeName = scnr.nextLine();
        
-        
+    
        do {           
            System.out.println("Would you like to enter an ingredient: (y or n)");
            String reply = scnr.next().toLowerCase();
+        //Here we create and if else statement to check for what decision the user wants to take.if they want to add and ingredient "y" we give them 
+        // that option,if not "n " we breake out of the loop
+            if (reply.equals("y")){// we use the equals method to compare the value of users reply with "y" which stand for yes
+             System.out.println("Please enter the name of the ingredient");//we then prompt the user to add the name of the ingredient he will like to add
+             Scanner scnrIngredient = new Scanner(System.in); // we get the added ingredient and assign it to scanner variable "scnrIngredient"
+             newIngredient = scnrIngredient.nextLine().toLowerCase();// we collect the scanner value and assign it the variable called "newIngredient"
+             ingredientList.add(newIngredient);//add the collected ingredient to the arrayList "ingredientList"
+
+           }else{
+            //this else statement will help us break out of the loop if user does not want to add an ingredient
+            addMoreIngredients = false;
+            break;
+           }
+ 
            
            /**
             * Add your code here (branches work well!).  The code should check the
@@ -39,13 +53,16 @@ public class SteppingStone4_Loops {
             
        } while (addMoreIngredients);
        
+       System.out.println("I am out of loop");
        for (int i = 0; i < ingredientList.size(); i++) {
            /** 
 		   * Get the item i from the ingredient list
 		   *	and assigning it to the String ingredient
 		   *
 		   */
-		   String ingredient = ???;
+          // Here we loop over the ingredientList arraylist and save the results into a variable called ingredient
+          // we then print each result as we loop through the various elements
+		   String ingredient = ingredientList.get(i);
            System.out.println(ingredient);
        }
     }
